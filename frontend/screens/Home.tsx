13d27955641a22a478732501a07d9e8bdd4081c5
Verneri <verneri.lopperi@gmail.com>
2025-01-15 15:55:25 +0200

    Button component

--- frontend/screens/Home.tsx
+++ frontend/screens/Home.tsx
@@ -1,9 +1,12 @@
 import {SafeAreaView} from 'react-native';
 import React from 'react';
-import globalStyle from '../assets/globalStyle';
+import globalStyle from '../assets/styles/globalStyle';
+import Button from '../components/Button/Button';
 
 export const Home = (): JSX.Element => {
   return (
-    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]} />
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <Button title={'Add task'} />
+    </SafeAreaView>
   );
 };
